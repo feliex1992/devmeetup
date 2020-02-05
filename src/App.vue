3d8594a24ea3c107030fbc60c06efb4c68b397d9
Jafar Pahrudin <jafar.pahrudin@gmail.com>
2020-02-05 18:40:37 +0700

    #1 first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        link
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+    <v-app-bar
+      app
+      dense
+      dark
+      class="purple darken-1"
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text small>
+          <v-icon left>mdi-account-multiple</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'View Meetups', icon: 'mdi-account-multiple' }
+    ]
+  })
+}
+</script>
