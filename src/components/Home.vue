66d36d590e82a346bea1a6cc1b90ca82fa129369
Jafar Pahrudin <jafar.pahrudin@gmail.com>
2020-02-07 19:52:20 +0700

    #4 edit home page 2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,13 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col cols="12">
-        <v-row
-          :align="alignment"
-          :justify="justify"
-          class="grey lighten-5"
-          style="height: 300px;"
-        >
-          <v-card
-            v-for="n in 3"
-            :key="n"
-            class="ma-3 pa-6"
-            outlined
-            tile
-          >
-            Column
-          </v-card>
-        </v-row>
-      </v-col>
-      <v-col cols="12">
-        <v-row justify="center">
-          <v-col
-            cols="6"
-            md="2"
-          >
-            <v-select
-              v-model="alignment"
-              :items="alignmentsAvailable"
-              label="Align"
-            ></v-select>
-          </v-col>
-
-          <v-col
-            cols="6"
-            md="2"
-          >
-            <v-select
-              v-model="justify"
-              :items="justifyAvailable"
-              label="Justify"
-            ></v-select>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
+  <v-container>
+    <v-layout row wrap class="primary">
+      <v-flex xs12 sm6 class="text-sm-center">
+        <v-btn large router to="/meetups">Explor Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-btn large router to="/meetup/new">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
