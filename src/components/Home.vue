f52f0e3415245f9456e1000b8ad34ba12fb33e99
feliex1992 <jafar.pahrudin@gmail.com>
2020-02-12 01:32:36 +0700

    #6 edit meetups

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,45 +1,45 @@
 <template>
   <v-container fluid>
-    <v-row wrap>
-      <v-col cols="12">
-        <v-row
-          justify="center"
+      <v-row
+        justify="center"
+        class="mt-4"
+      >
+        <v-card
+          class="mx-3">
+          <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+        </v-card>
+        <v-card
+          class="mx-3"
         >
-          <v-card
-            class="ma-2"
-          >
-            <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
-          </v-card>
-          <v-card
-            class="ma-2"
-          >
-            <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
-          </v-card>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row wrap justify="center" class="px-10">
-      <v-card max-width="444" class="mx-auto">
-        <v-carousel
-          cycle
-          show-arrows-on-hover
+          <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+        </v-card>
+      </v-row>
+      <v-row
+        wrap
+        justify="center"
+      >
+        <v-col
+          cols="12"
+          md="7"
         >
-          <v-carousel-item
-            v-for="item in items"
-            :key="item.id"
-            :src="item.src"
-          >
-            <v-row
-              class="fill-height"
-              justify="center"
-              align="end"
+          <v-row>
+            <v-carousel
+              cycle
+              show-arrows-on-hover
             >
-              <div class="display-1 mb-12">{{ item.title }}</div>
-            </v-row>
-          </v-carousel-item>
-        </v-carousel>
-      </v-card>
-    </v-row>
+              <v-carousel-item
+                v-for="item in items"
+                :key="item.id"
+                :src="item.src"
+              >
+                <v-row justify="center">
+                  <div class="title">{{ item.title }}</div>
+                </v-row>
+              </v-carousel-item>
+            </v-carousel>
+          </v-row>
+        </v-col>
+      </v-row>
   </v-container>
 </template>
 
@@ -57,3 +57,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0, 0, 0, 0.3);
+    color: white;
+    font-size: 2em;
+    padding: 10px;
+  }
+</style>
--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
-    <p>The Meetups page !</p>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col
+        md="12"
+      >
+        <v-card>
+          <v-row class="pl-3">
+            <v-col
+              cols="auto"
+              md="3"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+                height="130px"
+              ></v-img>
+            </v-col>
+            <v-col
+              cols="auto"
+              class="pt-0"
+            >
+              <v-card-title class="mt-0">
+                <div>
+                  <h5>Squirrel eat nut</h5>
+                  <div>
+                    <h6>28 February 2020</h6>
+                  </div>
+                </div>
+              </v-card-title>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
